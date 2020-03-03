7dd0c80b3deb641771d0e30a44ebb40b1f069f15
Brandon Liang <54skyxenon@gmail.com>
2020-03-03 14:12:17 -0500

    Configured all the routing

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -12,28 +7,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+// middleware setup
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// route for our homepage
+var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// route where we query the scraper
+var scraperRouter = require('./routes/scraper');
+app.use('/scraper', scraperRouter);
 
 module.exports = app;
