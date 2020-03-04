dd05c40ffde5fb07453f2531c3f6b2aeac584b73
Brandon Liang <54skyxenon@gmail.com>
2020-03-04 03:58:32 -0500

    Scraper can now add detailed info for each stop

--- routes/scraper.js
+++ routes/scraper.js
@@ -5,64 +5,140 @@ var router = express.Router();
 
 const puppeteer = require('puppeteer');
 
-// Returns data for the best flights
+// Returns the appropriate URL to scrape
+const getUrl = async (origin, dest, departDate, returnDate, isRoundTrip) => {
+    let baseUrl = `https://www.google.com/flights#flt=`
+
+    if (isRoundTrip) {
+        return baseUrl + `${origin}.${dest}.${departDate}*${dest}.${origin}.${returnDate}`;
+    } else {
+        return baseUrl + `${origin}.${dest}.${departDate};tt:o`;
+    }
+}
+
+// Clicks through dropdowns to reveal detailed stop data for the best flights
+const clickDropdowns = async (page) => {
+    await page.evaluate(async () => {
+        let elements = document.getElementsByClassName('gws-flights-results__best-flights')[0]
+            .getElementsByClassName('gws-flights-results__expand');
+        for (let element of elements) {
+            element.click();
+        }
+    });
+}
+
+// Scrapes Google Flights data (for only the best flights)
 const scrape = async (origin, dest, departDate, returnDate, isRoundTrip) => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-
     await page.setViewport({ width: 1920, height: 1080 });
 
-    if (isRoundTrip) {
-        await page.goto(`https://www.google.com/flights#flt=${origin}.${dest}.${departDate}*${dest}.${origin}.${returnDate}`,  
-        { waitUntil: 'networkidle0', timeout: 0 });
-    } else {
-        await page.goto(`https://www.google.com/flights#flt=${origin}.${dest}.${departDate};tt:o`,  
-        { waitUntil: 'networkidle0', timeout: 0 });
-    }
+    await page.goto(await getUrl(origin, dest, departDate, returnDate, isRoundTrip), { waitUntil: 'networkidle0', timeout: 0 })
 
+    await clickDropdowns(page);
+
+    // Sanity check - show us what we have so far
     await page.screenshot({ path: 'screenshot.png' });
-    console.log("screenshot taken!")
+    console.log('Screenshot taken!');
 
     const scrapedData = await page.evaluate(() => {
+        // get the elements in the ordered list of best flights
         let flights = Array.from(document.querySelectorAll('ol.gws-flights-results__result-list:nth-child(3) > li'));
-        
+
+        // get the prices
         const prices = flights.map(flight => {
             return flight.querySelector('div:nth-child(1) > div:nth-child(1) > div:nth-child(2) ' +
-                                        '> div:nth-child(2) > div:nth-child(1) > div:nth-child(6) ' +
-                                        '> div:nth-child(1)')
-            .textContent
-            .toString()
-            .trim()
+                '> div:nth-child(2) > div:nth-child(1) > div:nth-child(6) ' +
+                '> div:nth-child(1)')
+                .textContent
+                .toString()
+                .trim()
         });
 
+        // get the trip durations
         const durations = flights.map(flight => {
             return flight.querySelector('div:nth-child(1) > div:nth-child(1) > div:nth-child(2) ' +
-                                        '> div:nth-child(2) > div:nth-child(1) > div:nth-child(3) ' +
-                                        '> div:nth-child(1)')
-            .textContent
-            .toString()
-            .trim()
+                '> div:nth-child(2) > div:nth-child(1) > div:nth-child(3) ' +
+                '> div:nth-child(1)')
+                .textContent
+                .toString()
+                .trim()
         });
 
+        // get how many stops there are
         const stopCounts = flights.map(flight => {
-            const stopCount = flight.querySelector('div:nth-child(1) > div:nth-child(1) > ' +
-                                                'div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > ' + 
-                                                'div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > ' + 
-                                                'div:nth-child(1) > span:nth-child(1)')
-            .textContent
-            .toString()
-            return stopCount === "Nonstop" ? '0 stops' : stopCount
+            return flight.querySelector('div:nth-child(1) > div:nth-child(1) > ' +
+                'div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > ' +
+                'div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > ' +
+                'div:nth-child(1) > span:nth-child(1)')
+                .textContent
+                .toString()
         });
 
-        return [prices, durations, stopCounts];
+        /**
+         * -------------------------------------------
+         * Scrape detailed stop info, which includes:
+         * -------------------------------------------
+         * IATA codes of origin and destination airports
+         * Airline operator
+         * Flight Number
+         * Departure time
+         * Arrival time
+         * Travel duration
+         * 
+         */
+
+        const stops = flights.map(flight => {
+            const stopInfoSelectors = Array.from(flight.getElementsByClassName('gws-flights-results__leg'));
+
+            let stopInfos = [];
+
+            stopInfoSelectors.forEach(stopInfoSelector => {
+                const IATA = stopInfoSelector
+                    .getElementsByClassName('gws-flights-results__iata-code');
+                const airline = stopInfoSelector
+                    .getElementsByClassName('gws-flights-results__leg-flight')[0]
+                    .querySelector('div')
+                    .textContent;
+                const flightNumber = stopInfoSelector
+                    .getElementsByClassName('gws-flights-results__other-leg-info')[0]
+                    .querySelector('span > span:nth-child(1)')
+                    .textContent + stopInfoSelector
+                        .getElementsByClassName('gws-flights-results__other-leg-info')[0]
+                        .querySelector('span > span:nth-child(2)')
+                        .textContent;
+                const departureTime = stopInfoSelector
+                    .getElementsByClassName('gws-flights-results__leg-departure')[0]
+                    .querySelector('div')
+                    .textContent
+                    .trim();
+                const arrivalTime = stopInfoSelector
+                    .getElementsByClassName('gws-flights-results__leg-arrival')[0]
+                    .querySelector('div')
+                    .textContent
+                    .trim();
+                const travelDuration = stopInfoSelector
+                    .getElementsByClassName('gws-flights-results__leg-duration')[0]
+                    .querySelector('div > span')
+                    .textContent
+                    .trim();
+
+                stopInfos.push([IATA[0].textContent, IATA[1].textContent, airline, flightNumber, departureTime, arrivalTime, travelDuration]);
+            });
+
+            return stopInfos;
+        })
+
+        return [prices, durations, stopCounts, stops];
     });
 
-    console.log("The prices are: " + scrapedData[0].toString())
-    console.log("The travel durations are: " + scrapedData[1].toString())
-    console.log("The number of stops are: " + scrapedData[2].toString())
+    console.log("The prices are: " + scrapedData[0].toString());
+    console.log("The travel durations are: " + scrapedData[1].toString());
+    console.log("The number of stops are: " + scrapedData[2].toString());
+    console.log("The stops themselves are: " + scrapedData[3].toString());
 
-    await browser.close()
-    return scrapedData
+    await browser.close();
+    return scrapedData;
 }
 
 // Does data validation, receives the data, and renders it
