6a090f53e9dbcecb8ef2d0f9e962e8ff445af294
Brandon Liang <54skyxenon@gmail.com>
2020-03-04 23:54:50 -0500

    Added logic for actually inserting into the database

--- routes/db.js
+++ routes/db.js
@@ -6,22 +6,42 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Set up default mongoose connection
-const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0-78xbv.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0-78xbv.mongodb.net/Flights?retryWrites=true&w=majority`;
 mongoose.connect(uri, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// Get the model for representing trips
+// Get the models for representing queries and trips
+const Query = require('../models/Query');
 const Trip = require('../models/Trip');
 
-// Adds the flights to our local MongoDB instance
+// Adds the flights to our cloud MongoDB server
 const addFlights = async (flights) => {
+    // populate an array with trips we've gotten for our query
+    let trips = []
+
     for (var tripIndex = 0; tripIndex < flights[0].length; tripIndex++) {
-        console.log(`The price for trip ${tripIndex + 1} is: ` + flights[0][tripIndex].toString());
-        console.log(`The duration for trip ${tripIndex + 1} is: ` + flights[1][tripIndex].toString());
-        console.log(`The number of stops for trip ${tripIndex + 1} is: ` + flights[2][tripIndex].toString());
-        console.log(`The stops themselves for trip ${tripIndex + 1} are: ` + flights[3][tripIndex].toString());
+        const trip = new Trip({
+            price: flights[0][tripIndex].toString(),
+            duration: flights[1][tripIndex].toString(),
+            no_stops: flights[2][tripIndex].toString(),
+            stops: []
+        });
+
+        trip.save();
+        trips.push(trip._id);        
     }
+
+    const query = new Query({
+        origin: 'HND',
+        dest: 'JNB',
+        depart_date: new Date().getDate(),
+        return_date: new Date().getDate(),
+        is_round_trip: false,
+        trips: trips
+    });
+    query.save();
+    console.log('Successfully saved to DB!');
 }
 
 exports.addFlights = addFlights;
