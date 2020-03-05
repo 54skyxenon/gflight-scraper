568f80cf3c714a6e3302432c64af2e8a5d2c3160
Brandon Liang <54skyxenon@gmail.com>
2020-03-04 23:59:29 -0500

    Added favicon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 
 var app = express();
 
@@ -11,6 +12,7 @@ app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 // route for our homepage
 var indexRouter = require('./routes/index');
