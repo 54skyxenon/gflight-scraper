2d310fa30a66c67c785115287656f7656abda269
Brandon Liang <54skyxenon@gmail.com>
2020-03-06 04:40:54 -0500

    Finished testing for routing

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -3,7 +3,7 @@
 const app = require('../app');
 const request = require('supertest');
 
-jest.setTimeout(30000);
+jest.setTimeout(100000);
 
 describe('index route', () => {
     it('visiting the homepage works', async (done) => {
@@ -144,6 +144,138 @@ describe('scraper route', () => {
         
         expect(response.text
             .includes('Best results for'))
+            .toBe(true);
+
+        done();
+    });
+
+    it('round trip fails without return date', async (done) => {
+        const response = JSON.parse(
+            JSON.stringify(
+                await request(app)
+                    .post('/scraper')
+                    .send({
+                        origin: 'JFK', 
+                        dest: 'PEK', 
+                        departDate: '2020-03-16',
+                        returnDate: '',
+                        isRoundTrip: true
+                    })
+            )
+        );
+        
+        expect(response.text
+            .includes('You did not specify a return date for this round trip flight!'))
+            .toBe(true);
+
+        done();
+    });
+
+    it('round trip fails when return date doesnt come after departure date', async (done) => {
+        const response = JSON.parse(
+            JSON.stringify(
+                await request(app)
+                    .post('/scraper')
+                    .send({
+                        origin: 'JFK', 
+                        dest: 'PEK', 
+                        departDate: '2020-03-16',
+                        returnDate: '2020-03-13',
+                        isRoundTrip: true
+                    })
+            )
+        );
+        
+        expect(response.text
+            .includes('Your return date must be after your departure date!'))
+            .toBe(true);
+
+        done();
+    });
+
+    it('round trip succeeds #1', async (done) => {
+        const response = JSON.parse(
+            JSON.stringify(
+                await request(app)
+                    .post('/scraper')
+                    .send({
+                        origin: 'JFK', 
+                        dest: 'LAX', 
+                        departDate: '2020-03-19',
+                        returnDate: '2020-03-29',
+                        isRoundTrip: true
+                    })
+            )
+        );
+        
+        expect(response.text
+            .includes('Best results for'))
+            .toBe(true);
+
+        done();
+    });
+
+    it('round trip succeeds #2', async (done) => {
+        const response = JSON.parse(
+            JSON.stringify(
+                await request(app)
+                    .post('/scraper')
+                    .send({
+                        origin: 'BNA', 
+                        dest: 'PEK', 
+                        departDate: '2020-03-19',
+                        returnDate: '2020-04-01',
+                        isRoundTrip: true
+                    })
+            )
+        );
+        
+        expect(response.text
+            .includes('Best results for'))
+            .toBe(true);
+
+        done();
+    });
+
+    it('fails when you are too far into the future', async (done) => {
+        const response = JSON.parse(
+            JSON.stringify(
+                await request(app)
+                    .post('/scraper')
+                    .send({
+                        origin: 'BNA', 
+                        dest: 'PEK', 
+                        departDate: '2030-05-19',
+                        returnDate: '2030-12-01',
+                        isRoundTrip: true
+                    })
+            )
+        );
+        
+        expect(response.text
+            .includes('Your query returned no results.'))
+            .toBe(true);
+
+        done();
+    });
+
+    it('fails when IATA formatting is wrong', async (done) => {
+        const response = JSON.parse(
+            JSON.stringify(
+                await request(app)
+                    .post('/scraper')
+                    .send({
+                        origin: 'lorem', 
+                        dest: 'ipsum', 
+                        departDate: '2020-03-19',
+                        returnDate: '2020-03-25',
+                        isRoundTrip: true
+                    })
+            )
+        );
+        
+        expect(response.text
+            .includes('Your query returned no results.'))
             .toBe(true);
 
         done();
